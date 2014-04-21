dac6e02bd5a91f6179403d35b9913bd15dc840c4
linroex <linroex@coder.tw>
2014-04-21 12:46:49 +0800

    edit facebook share message

--- app/controllers/ContestantController.php
+++ app/controllers/ContestantController.php
@@ -7,7 +7,7 @@ class ContestantController extends Controller{
     }
     public function share(){
         $contestant = Contestant::data(Input::get('id'));
-        $message = "我剛剛在台科雞腿盃人氣獎網站投下神聖的一票\n我喜歡{$contestant->name}唱的{$contestant->department}";
+        $message = "我喜歡{$contestant->name}唱的{$contestant->department}\n大家快來雞腿盃人氣獎投票\n選出屬於你的歌王歌后！！";
         $link = url('contestant/' . Input::get('id'));
         User::post_facebook($message, $link);
         return '已將此訊息分享至您的塗鴉牆';
